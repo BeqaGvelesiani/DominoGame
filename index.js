e3f65d33077bbd44b0636e8372a60c6e3dde2885
Beqa Gvelesiani <beqagvelesiani1990@gmail.com>
2023-11-13 01:29:31 +0400

    dwew

--- index.js
+++ index.js
@@ -2,65 +2,13 @@ import { domino } from "./data.js"
 import { top } from "./data.js"
 import { bottom } from "./data.js"
 
-domino(3, 3)
 
-for(var i = 0; i<7; i++){
-    document.querySelector(".topButtons").innerHTML += `
-        <button id="topbtn${i}">top ${i}</button>
-    `
+function random(a,b){
+    let i = b-a+1;
+    return Math.floor(Math.random()*i) + a;
 }
 
-for(var i = 0; i<7; i++){
-    document.querySelector(".bottomButtons").innerHTML += `
-        <button id="bottombtn${i}">bottom ${i}</button>
-    `
-}
+let a =  random(0,6)
+let b =  random(0,6)
 
-document.getElementById(`topbtn0`).addEventListener("click", ()=>{
-    top(0)
-})
-
-document.getElementById(`topbtn1`).addEventListener("click", ()=>{
-    top(1)
-})
-
-document.getElementById(`topbtn2`).addEventListener("click", ()=>{
-    top(2)
-})
-
-document.getElementById(`topbtn3`).addEventListener("click", ()=>{
-    top(3)
-})
-
-document.getElementById(`topbtn4`).addEventListener("click", ()=>{
-    top(4)
-})
-
-document.getElementById(`topbtn5`).addEventListener("click", ()=>{
-    top(5)
-})
-
-document.getElementById(`topbtn6`).addEventListener("click", ()=>{
-    top(6)
-})
-
-document.getElementById(`bottombtn0`).addEventListener("click", ()=>{
-    bottom(0)})
-
-document.getElementById(`bottombtn1`).addEventListener("click", ()=>{
-    bottom(1)})
-
-document.getElementById(`bottombtn2`).addEventListener("click", ()=>{
-    bottom(2)})
-
-document.getElementById(`bottombtn3`).addEventListener("click", ()=>{
-    bottom(3)})
-
-document.getElementById(`bottombtn4`).addEventListener("click", ()=>{
-    bottom(4)})
-
-document.getElementById(`bottombtn5`).addEventListener("click", ()=>{
-    bottom(5)})
-
-document.getElementById(`bottombtn6`).addEventListener("click", ()=>{
-    bottom(6)})
+domino(a, b)
