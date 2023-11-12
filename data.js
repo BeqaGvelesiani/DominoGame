9e90c5fa03e6cc1e5445a194bb3e3876a7590efc
Beqa Gvelesiani <beqagvelesiani1990@gmail.com>
2023-11-12 23:42:30 +0400

    swqs

--- data.js
+++ data.js
@@ -1,3 +1,6 @@
+const top_part = document.getElementById("top")
+const bottom_part = document.getElementById("bottom")
+
 export const one = `
 <span class="off"></span>
 <span class="off"></span>
@@ -65,3 +68,33 @@ export const six = `
 <span class="off"></span>
 <span class="on"></span>
 `
+
+export function domino(a, b){
+    if(a==1){
+        top_part.innerHTML = one
+    }else if (a==2){
+        top_part.innerHTML = two
+    }else if (a==3){
+        top_part.innerHTML = three
+    }else if (a==4){
+            top_part.innerHTML = four
+    }else if (a==5){
+        top_part.innerHTML = five
+    }else{
+        top_part.innerHTML = six    
+    }
+
+    if(b==1){
+        bottom_part.innerHTML = one
+    }else if (b==2){
+        bottom_part.innerHTML = two
+    }else if (b==3){
+        bottom_part.innerHTML = three
+    }else if (b==4){
+        bottom_part.innerHTML = four
+    }else if (b==5){
+        bottom_part.innerHTML = five
+    }else{
+        bottom_part.innerHTML = six    
+    }
+}
--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-import { one } from "./data.js"
-import { two } from "./data.js"
-import { three } from "./data.js"
-import { four } from "./data.js"
-import { five } from "./data.js"
-import { six } from "./data.js"
+import { domino } from "./data.js"
 
-
-
-const top_part = document.getElementById("top")
-const bottom_part = document.getElementById("bottom")
-
-
-
-top_part.innerHTML = three
-bottom_part.innerHTML = six
-
-
-
-// console.log()
+domino(6, 6)
