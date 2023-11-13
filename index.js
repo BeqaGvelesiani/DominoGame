5e6a09cf24dc13a89028707b8c0769467bef38d7
BeqaGvelesiani <107481738+BeqaGvelesiani@users.noreply.github.com>
2023-11-13 12:58:10 +0400

    sd

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-import { domino } from "./data.js"
-import { top } from "./data.js"
-import { bottom } from "./data.js"
+import { domino } from "./data.js";
+import { top } from "./data.js";
+import { bottom } from "./data.js";
 
-
-function random(a,b){
-    let i = b-a+1;
-    return Math.floor(Math.random()*i) + a;
+function random(a, b) {
+  let i = b - a + 1;
+  return Math.floor(Math.random() * i) + a;
 }
 
-let a =  random(0,6)
-let b =  random(0,6)
+domino(0, 0)
 
-domino(a, b)
+document.getElementById("generate").addEventListener("click", ()=>{
+    let a = random(0, 6);
+    let b = random(0, 6);
+    domino(a, b)
+})
+
