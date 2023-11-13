d61dd2cd8d49bd0eab123aaf41a45db6564b44eb
BeqaGvelesiani <107481738+BeqaGvelesiani@users.noreply.github.com>
2023-11-13 17:05:36 +0400

    efg

--- data.js
+++ data.js
@@ -114,38 +114,22 @@ export function domino(a, b){
     }
 }
 
-export function top(a){
-    if(a==1){
-        top_part.innerHTML = one
-    }else if (a==2){
-        top_part.innerHTML = two
-    }else if (a==3){
-        top_part.innerHTML = three
-    }else if (a==4){
-        top_part.innerHTML = four
-    }else if (a==5){
-        top_part.innerHTML = five
-    }else if (a==0){
-        top_part.innerHTML = zero
-    }else{
-        top_part.innerHTML = six    
+export function number(a){
+    if (a == 1) {
+      return one;
+    } else if (a == 2) {
+      return two;
+    } else if (a == 3) {
+      return three;
+    } else if (a == 4) {
+      return four;
+    } else if (a == 5) {
+      return five;
+    } else if (a == 0) {
+      return zero;
+    } else {
+      return six;
     }
 }
 
-export function bottom(b){
-    if(b==1){
-        bottom_part.innerHTML = one
-    }else if (b==2){
-        bottom_part.innerHTML = two
-    }else if (b==3){
-        bottom_part.innerHTML = three
-    }else if (b==4){
-        bottom_part.innerHTML = four
-    }else if (b==5){
-        bottom_part.innerHTML = five
-    }else if (b==0){
-        bottom_part.innerHTML = zero
-    }else{
-        bottom_part.innerHTML = six    
-    }
-}
+
--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 import { domino } from "./data.js";
-import { top } from "./data.js";
-import { bottom } from "./data.js";
+import { number } from "./data.js";
 
 function random(a, b) {
   let i = b - a + 1;
   return Math.floor(Math.random() * i) + a;
 }
 
-domino(0, 0)
+domino(0, 0);
 
-document.getElementById("generate").addEventListener("click", ()=>{
-    let a = random(0, 6);
-    let b = random(0, 6);
-    domino(a, b)
-})
+let log = [];
 
+document.getElementById("generate").addEventListener("click", () => {
+  let a = random(0, 6);
+  let b = random(0, 6);
+  domino(a, b);
+  log.push([a, b]);
+  console.log(log);
+  renderLog(log);
+});
+
+function renderLog(arr) {
+
+      document.getElementById("logSection").innerHTML += `
+      <div class="domino" id="logDomino">
+          <div class="part part-top" id="">
+              ${number(arr[arr.length - 1][0])}
+          </div>
+          <div class="part part-bottom" id="">
+              ${number(arr[arr.length - 1][1])}
+          </div>
+        </div>
+      `;
+
+
+
+  // document.getElementById("logSection").innerHTML = ""
+  //   for (var i = 0; i < arr.length; i++) {
+  //     document.getElementById("logSection").innerHTML += `
+  //     <div class="domino" id="logDomino">
+  //         <div class="part part-top" id="">
+  //             ${number(arr[i][0])}
+  //         </div>
+  //         <div class="part part-bottom" id="">
+  //             ${number(arr[i][1])}
+  //         </div>
+  //       </div>
+  //     `;
+  //   }
+}
